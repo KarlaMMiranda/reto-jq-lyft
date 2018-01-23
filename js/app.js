3ff1e2610ac2196503c98ad196779e06fbf42374
KarlaMMiranda <charlotterougue@gmail.com>
2018-01-22 22:25:37 -0600

    Añade digitos aleatorios

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
-$(document).ready(function() {  //función que hace desaparecer mi imagen principal
+//Función que hace desaparecer mi imagen principal
+$(document).ready(function() {
   setTimeout(function() {
       $("#view-splash").fadeOut(1500);
     },2000);
 });
 
-$(document).ready(function() { //función que hace aparecer la siguiente pantalla
+//Función que hace aparecer la siguiente pantalla
+$(document).ready(function() {
     setTimeout(function() {
       $("#second-section").fadeIn(1500);
     },2000);
 });
+
+//Funcion que arroja los tres dígitos aleatorios
+function digitos() {
+    var x = Math.floor((Math.random() * 900) + 100);
+    document.getElementById("numbers").innerHTML = x;
+}
