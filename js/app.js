93a5b2cf63e8a21ae17d04bf3ef02c1cf010f074
KarlaMMiranda <charlotterougue@gmail.com>
2018-01-22 18:55:45 -0600

    actualiza vistas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log(restaurants);
+$(document).ready(function() {  //función que hace desaparecer mi imagen principal
+  setTimeout(function() {
+      $("#view-splash").fadeOut(1500);
+    },2000);
+});
+
+$(document).ready(function() { //función que hace aparecer la siguiente pantalla
+    setTimeout(function() {
+      $("#second-section").fadeIn(1500);
+    },2000);
+});
